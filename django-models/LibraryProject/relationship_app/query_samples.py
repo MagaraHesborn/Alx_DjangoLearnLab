0126dc699a3d0e0a5e0c04baf29c23501d78c459
Hesborn Magara <magarahesborn1@gmail.com>
2025-07-22 11:34:59 +0300

    changed querry structure

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,3 +1,11 @@
-books = Book.objects.filter(name=author_name)
-books = Library.objects.get(name=library_name)
+from relationship_app.models import Library, Book, Author, Librarian
+
+library_name = "Main Library"
+author_name = "Hesborn Magara"
+
+library = Library.objects.get(name=library_name)
+author = Author.objects.get(name=author_name)
+
+books = library.books.all()
 librarian = library.librarians
+books_by_author = author.books.all()
