3fc98bca397d4a4932c67293bc289a71a5c1a4b8
Hesborn Magara <magarahesborn1@gmail.com>
2025-07-23 14:15:42 +0300

    spelling mistake changes

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -3,7 +3,7 @@ from .models import Book, Library
 from django.views.generic import DetailView
 
 def book_list(request):
-    books = Book.objec+ts.all()
+    books = Book.objects.all()
     context = {'book_list': books}
     return render(request, 'relationship_app/list_books.html', context)
 
