addd8ca974daca057918f9a3d4f9e01b1b7648d8
Hesborn Magara <magarahesborn1@gmail.com>
2025-07-25 13:15:03 +0300

    made changes in urls.py

--- django-models/LibraryProject/LibraryProject/urls.py
+++ django-models/LibraryProject/LibraryProject/urls.py
@@ -21,10 +21,11 @@ from django.contrib.auth.views import LoginView, LogoutView
 
 
 urlpatterns = [
-    path('', include('bookshelf.urls')),
     path('', include('relationship_app.urls')),
     path('books/', include("bookshelf.urls")),
+
     path('admin/', admin.site.urls),
+
     path('login/', LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
 ]
