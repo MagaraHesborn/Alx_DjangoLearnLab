6a09d3dcd5f1165a62d3b24b31c799e3bfcd1734
Hesborn Magara <magarahesborn1@gmail.com>
2025-07-25 11:37:14 +0300

    modified SignUpView

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -5,7 +5,8 @@ from django.views.generic.detail import DetailView
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth import login
+from django. contrib.auth.views import LoginView, LogoutView
 
 def list_books(request):
     books = Book.objects.all()
@@ -20,6 +21,11 @@ class SignUpView(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'relationship_app/register.html'
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        login(self.request, self.object)
+        return response
 
 class LogInView(LoginView):
     template_name = 'relationship_app/login.html'
