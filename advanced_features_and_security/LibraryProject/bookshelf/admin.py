b49a81d122a2fa195bd8120f0589cc1eeb125a17
Hesborn Magara <magarahesborn1@gmail.com>
2025-07-31 10:19:45 +0300

    registred custom user model in admin.py

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,8 +1,25 @@
 from django.contrib import admin
-from .models import Book
+from .models import Book, CustomUser
+from django.contrib.auth.admin import UserAdmin
 
 class BookAdmin(admin.ModelAdmin):
     list_filter = ('title', 'author', 'publication_year')
     search_fields = ('title', 'author')
 
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+
+    fieldsets = UserAdmin.fieldsets + (
+        ("Additional Info", {
+            "fields": ("date_of_birth", "profile_photo",)
+        }),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ("Additional Info", {
+            "fields": ("date_of_birth", "profile_photo",)
+        }),
+    )
+
+
 admin.site.register(Book, BookAdmin)
+admin.site.register(CustomUser, CustomUserAdmin)
