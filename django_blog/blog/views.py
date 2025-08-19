d8c1b1df041b4af5b9c8d69c8def54a784470dd0
Hesborn Magara <magarahesborn1@gmail.com>
2025-08-19 20:25:17 +0300

    created authetication views and html templates

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from django.contrib.auth import login
+from django.contrib.auth.views import LoginView, LogoutView
+
+
+class Register(CreateView):
+    form_class = UserCreationForm()
+    success_url = reverse_lazy
+    template_name = 'blog/register.html'
+
+    def form_invalid(self, form):
+        response = super().form_valid(form)
+
+        login(self.request, self.object)
+        return response
+    
+class LoginView(LoginView):
+    template_name = 'blog/login.html'
+
+class LogoutView(LogoutView):
+    next_page = 'login'
 
 # Create your views here.
