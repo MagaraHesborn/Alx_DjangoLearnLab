38c769d3576c07ef643edc38afb0c0422730edc4
Hesborn Magara <magarahesborn1@gmail.com>
2025-08-21 12:12:14 +0300

    implemented mixins for post creation, update and delete

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.views.generic import ListView, DetailView, DeleteView, UpdateView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
@@ -72,24 +73,36 @@ class PostDetailView(DetailView):
     context_object_name = "post"
 
 
-class PostCreateView(CreateView):
+class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
     template_name = "blog/post_form.html"
     fields = ['title, content, published_date, author']  
     success_url = reverse_lazy('post-list')
 
+    def form_valid(self, form):
+        form.instance.author = self.request.user  # ✅ auto-assign post to logged-in user
+        return super().form_valid(form)
 
-class PostUpdateView(UpdateView):
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
     template_name = "blog/post_form.html"
     fields = ['title, content, published_date, author']
     success_url = reverse_lazy('post-list')
 
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author  # ✅ only author can edit post
 
-class PostDeleteView(DeleteView):
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Post
     template_name = "blog/post_confirm_delete.html"
     success_url = reverse_lazy('post-list')
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author  # ✅ only author can delete post
 
 
 # Create your views here.
