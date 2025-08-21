9cf562e340985638fe8c7142b1e932cbe1808a4a
Hesborn Magara <magarahesborn1@gmail.com>
2025-08-21 17:33:21 +0300

    created a form for comments

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.contrib.auth.models import User
-from .models import Profile
+from .models import Profile, Comment
 
 class UserUpdateForm(forms.ModelForm):
     class Meta:
@@ -11,3 +11,14 @@ class ProfileUpdateForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ['bio', 'profile_picture']
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['content']
+
+    def clean_content(self):
+        content = self.cleaned_data.get('content')
+        if not content or content.strip() == "":
+            raise forms.ValidationError("Comment cannot be empty")
+        return content
--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -25,4 +25,4 @@ class Comment(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
