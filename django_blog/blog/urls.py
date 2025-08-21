6a60bb08c95c0107666cb45a2eef83cd6b927a94
Hesborn Magara <magarahesborn1@gmail.com>
2025-08-21 10:41:19 +0300

    updated blog/urls.py

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import Register, BlogLoginView, BlogLogoutView, profile, home, posts
+from .views import ListView, DeleteView, CreateView, DetailView, UpdateView
 
 urlpatterns = [
     path('', home, name='home'),
@@ -7,5 +8,11 @@ urlpatterns = [
     path('login/', BlogLoginView.as_view(), name='login'),
     path('logout/', BlogLogoutView.as_view(), name='logout'),
     path('profile/', profile, name='profile'),
-     path('posts/', posts, name='posts'),
+    path('posts/', posts, name='posts'),
+
+    path('posts/', ListView.as_view(), name='post-list'),
+    path('posts/detail/', DetailView.as_view(), name='post-list'),
+    path('posts/create/', CreateView.as_view(), name='post-list'),
+    path('postsdelete//', DeleteView.as_view(), name='post-list'), 
+    path('posts/update/', UpdateView.as_view(), name='post-list'),
 ]
